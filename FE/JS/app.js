1ab3a3d81e597f99c0bad55125b2ca3c2f819da1
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-06-13 17:40:19 +0900

    create login modal

--- FE/JS/app.js
+++ FE/JS/app.js
@@ -1,6 +1,13 @@
 let modal = false;
 
+const mainBlur = () => {
+    document.querySelector(".bar").classList.toggle('blur');
+    document.querySelector('.greeting').classList.toggle('blur');
+    document.querySelector('.lottie-flying').classList.toggle('blur');
+}
 
 const modalOnOff = () => {
-    document.querySelector(".bar").classList.toggle('blur');
+    modal = !modal;
+    mainBlur();
+    document.querySelector('.sign-in').classList.toggle('hidden');
 }
