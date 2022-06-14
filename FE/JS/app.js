a1eddb69681c248d57fbb744203b7b96574c6328
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-06-15 00:53:17 +0900

    finish sign-up layout

--- FE/JS/app.js
+++ FE/JS/app.js
@@ -6,8 +6,25 @@ const mainBlur = () => {
     document.querySelector('.lottie-flying').classList.toggle('blur');
 }
 
-const modalOnOff = () => {
-    modal = !modal;
+const signInclose = () => {
     mainBlur();
     document.querySelector('.sign-in').classList.toggle('hidden');
 }
+
+const signUpclose = () => {
+    mainBlur();
+    document.querySelector('.sign-up').classList.toggle('hidden');
+}
+
+const signInOnOff = () => {
+    mainBlur();
+    document.querySelector('.sign-in').classList.toggle('hidden');
+}
+
+const signUpOnOff = () => {
+    document.querySelector('.sign-in').classList.toggle('hidden');
+    document.querySelector('.sign-up').classList.toggle('hidden');
+}
+
+
+
