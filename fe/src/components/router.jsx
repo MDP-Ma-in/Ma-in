be67129c97a6054920f66c4f4d8c817e0cab34e3
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-07-26 03:19:54 +0900

    add new environment creation

--- fe/src/components/router.jsx
+++ fe/src/components/router.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import React , {} from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Auth from './Auth/Auth';
 import Home from './Home/Home';
@@ -7,12 +7,14 @@ import Nav from './Nav/Nav';
 
 const Router = () => {
   return (
+    <>
     <BrowserRouter>
     <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/login' element={<Auth/>}/>
     </Routes>
     </BrowserRouter>
+    </>
   )
 }
 
